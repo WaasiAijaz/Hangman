f0d4080ffa3226da1eee583f3e8e0236479a57c2
WaasiAijaz <waasiaijaz786@gmail.com>
2025-10-22 09:54:22 -0400

    Added image update functionality

--- hangman.js
+++ hangman.js
@@ -1,16 +1,22 @@
 var POSSIBLE_WORDS = ["obdurate", "verisimilitude", "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
 var word = "";
-var guesses =""
+var guesses ="";
+var MAX_GUESSES = 6;
+var guess_count = MAX_GUESSES;
 
 function newGame(){
     var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
     guesses = " ";
+    guess_count = MAX_GUESSES;
     updatePage();
 }
 function guessLetter(){
     var input = document.getElementById("guess");
-    var letter = input.value; 
+    var letter = input.value;
+    if(word.indexOf(letter)< 0){
+        guess_count--;
+    } 
     guesses+=letter;
     updatePage();
 
@@ -31,4 +37,7 @@ function updatePage(){
     clue.innerHTML = clueString;
     var guessArea = document.getElementById("guesses");
     guessArea.innerHTML = "Guessed Letters: " + guesses;
+    var image = document.getElementById("hangmanImage");
+    image.src = "images/hangman" + guess_count +".gif";
+
 }
