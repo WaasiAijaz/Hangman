9cfb15c01d0e0f88e7f65bc4954f02a0f71c98a1
WaasiAijaz <waasiaijaz786@gmail.com>
2025-11-02 01:08:56 -0500

    Added code to only guess a letter once you hit new game

--- hangman.js
+++ hangman.js
@@ -3,22 +3,35 @@ var word = "";
 var guesses ="";
 var MAX_GUESSES = 6;
 var guess_count = MAX_GUESSES;
+var startStop = false;
+var gameOver = false;
 
 function newGame(){
     var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
     guesses = " ";
     guess_count = MAX_GUESSES;
+    startStop = true;
+    gameOver = false;
     updatePage();
+    document.getElementById("guesses").innerHTML = "Guessed Letters: ";
+    document.getElementById("clue"),innerHeight = "_ ".repeat(word.length);
 }
 function guessLetter(){
     var input = document.getElementById("guess");
     var letter = input.value;
+    input.value = "";
+    if(!startStop){
+        updatePage();
+        return;
+    }
+
     if(word.indexOf(letter)< 0){
         guess_count--;
     } 
     guesses+=letter;
     updatePage();
+    input.value = "";
 
 }
 function updatePage(){
